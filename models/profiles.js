c0a51eb8ed09f7903260679b894cbbbadc935f0b
Tetra <seifabziz@gmail.com>
2019-01-23 07:18:07 -0800

    Epic

--- models/profiles.js
+++ models/profiles.js
@@ -5,7 +5,12 @@ const profileSchema = mongoose.Schema({
   wiiPoints: Number,
   bio: String,
   totalPosts: Number,
+<<<<<<< HEAD
   blacklisted: Boolean,
+=======
+  items: Array,
+  badges: Array
+>>>>>>> a3f89a5a8ba421bfc54be3916fd55e08b3b7a6d3
 });
 
 module.exports = mongoose.model("profiles", profileSchema);
