769bcfa72b2757d28d59f136cb63389f85bfa496
Tetra <seifabziz@gmail.com>
2019-01-25 15:40:41 -0800

    op command

---
+++ commands/op.js
@@ -0,0 +1,63 @@
+const Discord = require("discord.js");
+const { invisible } = require("../data/colors.json");
+const { loading } = require("../data/emojis.json");
+const { currency, owner } = require("../settings.json");
+const replies = require("../data/replies.json");
+const profiles = require("../models/profiles.js");
+const mongoose = require("mongoose");
+const mongoUrl = require("../tokens.json").mongodb;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true
+});
+
+module.exports = {
+  name: "op",
+  description: "Give/take money from a user.",
+  usage: "<user> <set/take/give> <amount>",
+  args: true,
+  async execute (client, message, args) {
+    if (!owner.includes(message.author.id)) return message.channel.send(replies.noPerms);
+    const msg = await message.channel.send(`${loading} Editing profile...`);
+
+    if((args[1].toLowerCase() !== "set" && args[1].toLowerCase() !== "take" && args[1].toLowerCase() !== "give") || !args[1]) return msg.edit("Please privde an action. Actions: set, take, give.")
+    if(!args[2]) return msg.edit("Please provide an amount.");
+
+    const user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+    if (!user) return msg.edit(replies.noUser);
+
+    let action = args[1].toLowerCase();
+    let amount = args[2].toLowerCase();
+    if(isNaN(amount)) return msg.edit("The amount you provided was not a number.");
+
+    profiles.findOne({
+      authorID: user.id
+    }, async (err, u) => {
+      if (err) console.log(err);
+
+      if (!u) {
+        const newUser = new profiles({
+          authorID: user.id,
+          wiiPoints: 0,
+          bio: "No bio set",
+          totalPosts: 0,
+          blacklisted: false,
+          voted: false,
+          supporter: false,
+          supporterr: false,
+          supporterrr: false,
+          mod: false,
+          developer: false,
+        });
+        await newUser.save().catch(e => console.log(e));
+      }
+
+      if(action === "set") u.wiiPoints = amount;
+      if(action === "give") u.wiiPoints += amount;
+      if(action === "take") u.wiiPoints -= amount;
+      await u.save().catch(e => console.log(e));
+
+      return msg.edit(`**${user.user.tag}** now has ${u.wiiPoints} ${currency}.`)
+    });
+  },
+};
