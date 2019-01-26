67afbc2c2003919f80b3f20cd9d6dceccca53ceb
Tetra <seifabziz@gmail.com>
2019-01-26 07:24:57 -0800

    More bug fixes

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 const Discord = require ("discord.js"); // eslint-disable-line no-unused-vars
 const { server, currency, prefix } = require("../settings.json");
+const { typing } = require("../data/emojis.json")
 
 module.exports = {
   name: "help",
@@ -7,14 +8,15 @@ module.exports = {
   aliases: ["commands"],
   async execute (client, message, args) {
       
-    // const user = message.guild.members.get(args[0]) || message.member;      
+    const msg = await message.channel.send(`${typing} Sending a list of my commands...`);
 
+    const user = message.member;    
     const { commands } = message.client;
     const data = [];
 
-    if (!args[0]) {
-      return message.channel.send(`
-**List of available commands**
+		if (!args.length) {
+      let helpStr = 
+`**List of available commands**
 
 Type \`${prefix}<command>\` to use a command. 
 To get more info on a specific command do \`${prefix}help <command>\`
@@ -32,13 +34,19 @@ To get more info on a specific command do \`${prefix}help <command>\`
 **stats** - displays the bot's stats
 **upload** - upload a meme to the database
 
-Need more help? Join the support server: ${server}
-            `);
+Need more help? Join the support server: ${server}`;
+
+      try{
+          await user.send(helpStr);
+          msg.edit(`Sent you a dm with my commands <@${message.author.id}>!`)
+      } catch (e) {
+          message.reply("Your dms are disabled, I don't have permission to send you commands.")
+      }
     } else {
+
       if (!commands.has(args[0])) {
         return message.reply("That's not a valid command!");
       }
-
       const command = commands.get(args[0]);
 
       data.push(`**Name:** ${command.name}`);
