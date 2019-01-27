8f60ed09510dd16f3471afc28a62e5fa1d198bcf
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-01-27 21:27:31 +0200

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js"); // eslint-disable-line no-unused-vars
 const { loading } = require("../data/emojis.json");
 const { logs } = require("../data/channels.json");
-const { owner, currency } = require("../settings.json");
+const { owner, currency, devs } = require("../settings.json");
 const profiles = require("../models/profiles.js");
 const replies = require("../data/replies.json");
 const mongoose = require("mongoose");
@@ -18,7 +18,7 @@ module.exports = {
   usage: "<user> <group(supporter, mod)>",
   args: true,
   async execute (client, message, args) {
-    if (!owner.includes(message.author.id)) return message.channel.send(replies.noPerms);
+    if (!devs.includes(message.author.id)) return message.channel.send(replies.noPerms);
     if(!args[0]) return message.channel.send("Please specifiy a user.")
     if(!args[1]) return message.channel.send("Please specify a group to add the user to.");
     if(args[1].toLowerCase() !== "supporter" && args[1].toLowerCase() !== "mod" && args[1].toLowerCase() !== "developer" && args[1].toLowerCase() !== "voter") return message.channel.send("That's not a valid group. Valid groups: supporoter, mod, voter, developer.")
