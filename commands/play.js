20114300a05c826a3ae1ea683623ac33b7c437cd
Tetra <seifabziz@gmail.com>
2019-01-30 08:25:10 -0800

    Works with urls

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validUrl = require("valid-url");
 const mongoUrl = require("../tokens.json").mongodb;
 const { loading } = require("../data/emojis.json");
 const Discord = require("discord.js");
@@ -16,16 +17,24 @@ module.exports = {
 
     const msg = await message.channel.send(`${loading} Joining vc...`);
     try {
-        const mp3 = message.attachments.first().url;
-        const connection = await message.member.voiceChannel.join();
-        const voiceChannel = message.member.voiceChannel;
-        const dispatcher = connection.playStream(mp3);
-        msg.edit("Now Playing MP3")
-        dispatcher.on('end', () => {
-            voiceChannel.leave();
-            msg.edit("MP3 Ended");
-        });
+      let mp3;
+      if(!message.attachments.first()){
+        if(args[0] && validUrl.isUri(args[0])){
+          mp3 = args[0];
+        }
+      } else {
+        mp3 = message.attachments.first().url;
+      }
+      const connection = await message.member.voiceChannel.join();
+      const voiceChannel = message.member.voiceChannel;
+      const dispatcher = connection.playStream(mp3);
+      msg.edit("Now Playing MP3")
+      dispatcher.on('end', () => {
+          voiceChannel.leave();
+          msg.edit("MP3 Ended");
+      });
     } catch(e) {
+      console.log(e);
         return msg.edit("An error occured. Please make sure you are playing an MP3 file!")
     }
   },
