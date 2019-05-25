2aca4a6c3168a8256bf7beadeb7a7a34f3e0641c
Tetra <seifabziz@gmail.com>
2019-05-25 08:19:15 -0700

    fixed bio bug

--- commands/user/bio.js
+++ commands/user/bio.js
@@ -20,34 +20,41 @@ module.exports = {
     const msg = await message.channel.send(`${loading} Setting bio...`);
     if (args.join(" ").length > 200) return msg.edit(replies.bioLong);
 
-    profiles.findOne({
-      authorID: message.author.id
-    }, async (err, user) => {
-      if (err) console.log(err);
-
-      if (!user) {
-        const newUser = new profiles({
-          authorID: user.id,
-          bytes: 0,
-          multiplier: false,
-          bio: args.join(" "),
-          totalPosts: 0,
-          blacklisted: false,
-          voted: false,
-          supporter: false,
-          supporterr: false,
-          supporterrr: false,
-          mod: false,
-          developer: false,          
-        });
-
-        await newUser.save().catch(e => console.log(e));
+    try {
+      profiles.findOne({
+        authorID: message.author.id
+      }, async (err, user) => {
+        if (err) console.log(err);
+  
+        if (!user) {
+          const newUser = new profiles({
+            authorID: message.author.id,
+            bytes: 0,
+            multiplier: false,
+            bio: args.join(" "),
+            totalPosts: 0,
+            blacklisted: false,
+            voted: false,
+            supporter: false,
+            supporterr: false,
+            supporterrr: false,
+            mod: false,
+            developer: false,          
+          });
+  
+          await newUser.save().catch(e => console.log(e));
+          return msg.edit(`Succesfully set your bio to \`${args.join(" ")}\``);
+        }
+  
+        user.bio = args.join(" ");
+        user.save().catch(e => console.log(e));
         return msg.edit(`Succesfully set your bio to \`${args.join(" ")}\``);
-      }
-
-      user.bio = args.join(" ");
-      user.save().catch(e => console.log(e));
-      return msg.edit(`Succesfully set your bio to \`${args.join(" ")}\``);
-    });
+      });
+    } catch (e) {
+      msg.delete();
+      client.commands.get("bio").execute(client, message, args);
+      console.log(e);
+      return;
+    }
   },  
 };
