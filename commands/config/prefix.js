f0c10f47820adb94d1cc9b5a86412cd5c6ec7d28
Tetra <seifabziz@gmail.com>
2019-05-25 18:05:53 -0700

    Custom prefix and blacklist and whitelist commands

--- commands/config/prefix.js
+++ commands/config/prefix.js
@@ -1,48 +1,63 @@
-// const Discord = require("discord.js"); // eslint-disable-line no-unused-vars
-// const { typing } = require("../../data/emojis.json");
-// const { invisible } = require("../../data/colors.json");
-// const replies = require("../../data/replies.json");
-// const servers = require("../models/server.js");
-// const mongoose = require("mongoose");
-// const mongoUrl = require("../..//tokens.json").mongodb;
+const Discord = require("discord.js"); // eslint-disable-line no-unused-vars
+const { typing } = require("../../data/emojis.json");
+const { invisible } = require("../../data/colors.json");
+const replies = require("../../data/replies.json");
+const servers = require("../../models/server.js");
+const mongoose = require("mongoose");
+const mongoUrl = require("../../tokens.json").mongodb;
 
-// mongoose.connect(mongoUrl, {
-//   useNewUrlParser: true
-// });
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true
+});
 
-// module.exports = {
-//   name: "prefix",
-//   description: "Set the prefix for the server",
-//   usage: "<prefix>",
-//   aliases: ['setprefix'],
-//   async execute (client, message, args) {
-//     if(!args[0]) {
-//         servers.findOne({
-//             serverID: message.guild.id
-//         }, async (err, s) => {
-//             if (err) console.log(err);
-//             return message.channel.send(`The current prefix is ${s.prefix}`);
-//         });
-//     }
-//     if(!args[0]) return;
+module.exports = {
+  name: "prefix",
+  description: "Set the prefix for the server",
+  usage: "<prefix>",
+  aliases: ['setprefix'],
+  async execute (client, message, args) {
+    if(!args[0]) {
+        servers.findOne({
+            serverID: message.guild.id
+        }, async (err, s) => {
+            if (err) console.log(err);
+            if (!s) {
+                const newSever = new servers({
+                  serverID: message.guild.id,
+                  prefix: "`e.`",                    
+                });
+                await newSever.save().catch(e => console.log(e));
+            }
+            return message.channel.send(`The current prefix is ${s.prefix}`);
+        });
+    }
+    if(!args[0]) return;
 
-//     if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(replies.noPerms);
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(replies.noPerms);
 
-//     let f = args[0].replace("_", " ");
-//     const msg = await message.channel.send(`${typing} Settings prefix to ${f}...`);
+    let f = args[0].replace("_", " ");
+    const msg = await message.channel.send(`${typing} Setting prefix to ${f}...`);
 
-//     servers.findOne({
-//         serverID: message.guild.id
-//     }, async (err, s) => {
-//         if (err) console.log(err);
-//         s.prefix = f;
-//         await s.save().catch(e => console.log(e));
-//         const embed = new Discord.RichEmbed()
-//         .setAuthor(`${message.guild.name}`, message.guild.iconURL)
-//         .setColor(invisible)
-//         .setDescription(`Successfully set the prefix to \`${f}\``)
-//         .setFooter(`If you were planning on adding a space at the end, substitue it with _ instead`);
-//         msg.edit(embed);
-//     });
-//   },
-// };
+    servers.findOne({
+        serverID: message.guild.id
+    }, async (err, s) => {
+        if (err) console.log(err);
+        if (!s) {
+            const newSever = new servers({
+              serverID: message.guild.id,
+              prefix: "e.",                    
+            });
+            await newSever.save().catch(e => console.log(e));
+        }
+
+        s.prefix = f;
+        await s.save().catch(e => console.log(e));
+        const embed = new Discord.RichEmbed()
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL)
+        .setColor(invisible)
+        .setDescription(`Successfully set the prefix to \`${f}\``)
+        .setFooter(`If you were planning on adding a space, substitue it with _ instead`);
+        msg.edit(embed);
+    });
+  },
+};
--- models/channel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const channelSchema = mongoose.Schema({
-  channelID: Number,
-  ignore: Boolean,
-});
-
-module.exports = mongoose.model("channels", channelSchema);
