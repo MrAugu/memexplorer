c97a922295ae484ca06e1bd0666cce022fb80f82
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-06-01 22:35:09 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = class {
     console.log(`${this.client.user.username} is online. Running on ${this.client.guilds.size} servers`);
     this.client.channels.get(ready).send(`${this.client.user.username} has restarted. Running on \`${this.client.guilds.size}\` servers.`);
     
-    const memeCount = await posts.countDocuments();
+    const memeCount = await posts.countDocuments({ approved: true });
     await this.client.user.setActivity(`with ${memeCount} memes`);
     
     const dblHandler = require("../dbl.js");
